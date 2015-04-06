f6acbc4d5f0d31b09b1b7012d02f484394968490
AJcodes <adeljo@hotmail.com>
2015-04-06 22:33:07 +0800

    Code Refactored
    Minor Code Refactoring

--- main.cpp
+++ main.cpp
@@ -2,30 +2,27 @@
 #include <QDesktopWidget>
 #include <QApplication>
 
-void center(QWidget &widget)
-{
-  int x, y;
-  int screenWidth;
-  int screenHeight;
+void center(QWidget &widget) {
+    int x, y;
+    int screenWidth;
+    int screenHeight;
 
-  int WIDTH = 300;
-  int HEIGHT = 400;
+    int WIDTH = 300;
+    int HEIGHT = 400;
 
+    QDesktopWidget *desktop = QApplication::desktop();
 
-  QDesktopWidget *desktop = QApplication::desktop();
+    screenWidth = desktop->width();
+    screenHeight = desktop->height();
 
-  screenWidth = desktop->width();
-  screenHeight = desktop->height();
+    x = (screenWidth - WIDTH) / 2;
+    y = (screenHeight - HEIGHT) / 2;
 
-  x = (screenWidth - WIDTH) / 2;
-  y = (screenHeight - HEIGHT) / 2;
-
-  widget.setGeometry(x, y, WIDTH, HEIGHT);
-  widget.setFixedSize(WIDTH, HEIGHT);
+    widget.setGeometry(x, y, WIDTH, HEIGHT);
+    widget.setFixedSize(WIDTH, HEIGHT);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     Game window;
